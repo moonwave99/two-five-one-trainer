6f62553bd7a014ce870a0081ebf376b161493aeb
moonwave99 <diego.caponera@gmail.com>
2026-02-15 15:27:44 +0100

    Update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect, useContext } from "react";
 import { nanoid } from "nanoid";
 import {
-    getByInterval,
-    getTwoFiveOne,
-    getEnharmonic,
-    INTERVALS,
-    shortcuts,
-    setSearchParams,
+  getByInterval,
+  getTwoFiveOne,
+  getEnharmonic,
+  INTERVALS,
+  shortcuts,
+  setSearchParams,
 } from "./lib";
 
 import { SettingsContext } from "./SettingsProvider";
@@ -15,76 +15,68 @@ import BackingTracks from "./BackingTracks";
 import Card from "./Card";
 
 function App() {
-    const [cards, setCards] = useState([]);
-    const [showSettings, setShowSettings] = useState(false);
-    const { settings, setSettings } = useContext(SettingsContext);
+  const [cards, setCards] = useState([]);
+  const { settings, setSettings } = useContext(SettingsContext);
 
-    useEffect(() => {
-        const { cycle: amount } = INTERVALS.find(
-            ({ name }) => name === settings.interval
-        );
-        setCards(
-            getByInterval({ amount, ...settings }).map((root) => ({
-                notes: getTwoFiveOne(getEnharmonic(root, settings.mode)),
-                id: nanoid(),
-            }))
-        );
-        setSearchParams(settings);
-    }, [settings]);
-
-    return (
-        <>
-            <header>
-                <h1>Dr. Drill</h1>
-                <div className="controls">
-                    <ul className="favorites">
-                        {shortcuts.map(({ label, settings }, index) => (
-                            <li key={index}>
-                                <button
-                                    onClick={() =>
-                                        setSettings((prev) => ({
-                                            ...prev,
-                                            ...settings,
-                                        }))
-                                    }
-                                >
-                                    {label}
-                                </button>
-                            </li>
-                        ))}
-                        <li>
-                            <button
-                                onClick={() => setShowSettings((prev) => !prev)}
-                            >
-                                Toggle Settings
-                            </button>
-                        </li>
-                    </ul>
-                    {showSettings && <Form />}
-                </div>
-            </header>
-
-            <main>
-                <div className="cards">
-                    {cards.slice(0, settings.max).map((card) => (
-                        <Card key={card.id} {...card} settings={settings} />
-                    ))}
-                </div>
-                <BackingTracks />
-            </main>
-            <footer>
-                &copy; {new Date().getFullYear()}{" "}
-                <a
-                    href="https://github.com/moonwave99"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    mwlabs
-                </a>
-                .
-            </footer>
-        </>
+  useEffect(() => {
+    const { cycle: amount } = INTERVALS.find(
+      ({ name }) => name === settings.interval,
     );
+    setCards(
+      getByInterval({ amount, ...settings }).map((root) => ({
+        notes: getTwoFiveOne(getEnharmonic(root, settings.mode)),
+        id: nanoid(),
+      })),
+    );
+    setSearchParams(settings);
+  }, [settings]);
+
+  return (
+    <div className="app">
+      <section className="sidebar">
+        <h1>Dr. Drill</h1>
+        <div className="controls">
+          <ul className="favorites">
+            {shortcuts.map(({ label, settings }, index) => (
+              <li key={index}>
+                <button
+                  onClick={() =>
+                    setSettings((prev) => ({
+                      ...prev,
+                      ...settings,
+                    }))
+                  }
+                >
+                  {label}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <Form />
+          <footer>
+            &copy; {new Date().getFullYear()}{" "}
+            <a
+              href="https://github.com/moonwave99"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              mwlabs
+            </a>
+            .
+          </footer>
+        </div>
+      </section>
+
+      <main>
+        <div className="cards">
+          {cards.slice(0, settings.max).map((card) => (
+            <Card key={card.id} {...card} settings={settings} />
+          ))}
+        </div>
+        <BackingTracks />
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/BackingTracks.jsx
+++ src/BackingTracks.jsx
@@ -1,33 +1,39 @@
 import { useState } from "react";
 import ReactPlayer from "react-player/youtube";
-import backingTracks from './backingTracks.json';
+import backingTracks from "./backingTracks.json";
 
 const entries = Object.entries(backingTracks);
 
 export default function BackingTracks() {
-    const [video, setVideo] = useState(entries[0][1][0].href);
-    return (
-        <section className="backing-tracks">
-            <h2>Backing Tracks</h2>
-            <label>
-                Choose Track:
-                <select onChange={(event) => {
-                    const [i,j] = event.target.value.split(',').map(Number);
-                    setVideo(entries[i][1][j]?.href)
-                }}>
-                    {entries.map(([group, entries], i) => <optgroup key={group} label={group}>
-                        {entries.map(({ label, href }, j) => (
-                            <option key={href} value={[i,j]}>{label}</option>
-                        ))}
-                    </optgroup>)}
-                </select>
-            </label>
-            <ReactPlayer
-                url={video}
-                className="player"
-                height={150}
-                controls={true}
-            />
-        </section>
-    );
+  const [video, setVideo] = useState(entries[0][1][0].href);
+  return (
+    <section className="backing-tracks">
+      <h2>Backing Track</h2>
+      <label>
+        Choose Track:
+        <select
+          onChange={(event) => {
+            const [i, j] = event.target.value.split(",").map(Number);
+            setVideo(entries[i][1][j]?.href);
+          }}
+        >
+          {entries.map(([group, entries], i) => (
+            <optgroup key={group} label={group}>
+              {entries.map(({ label, href }, j) => (
+                <option key={href} value={[i, j]}>
+                  {label}
+                </option>
+              ))}
+            </optgroup>
+          ))}
+        </select>
+      </label>
+      <ReactPlayer
+        url={video}
+        className="player"
+        height={230}
+        controls={true}
+      />
+    </section>
+  );
 }
